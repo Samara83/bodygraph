e2f773052479517afd3fbdf2d0e06d402fc086f3
Samara83 <hello@sambishop.co>
2026-05-22 11:49:29 +0500

    index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header('Content-Type: application/json');
 
 /*
